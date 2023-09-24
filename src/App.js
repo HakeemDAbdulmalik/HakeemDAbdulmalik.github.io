1b1de45fe31a16f8aecc4714735fafebfc45d4cc
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2023-09-24 18:39:35 -0400

    Add profile.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './pageBlocks/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hakeem Abdulmalik</p>
+        <p>Freelance Software Engineer</p>
+
+        <p>I build interesting projects. I specialize in automation, optimization, and data analysis</p>
+        <ul>
+          <li>Github icon</li>
+          <li>LinkedIn icon</li>
+        </ul>
       </header>
+
+      <About></About>
     </div>
   );
 }
+
+
+              /* background:radial-gradient(
+              600px circle at 0px 0px,
+              rgba(29, 78, 216, 0.15),
+              transparent 80%
+            ); */
+            // Background color
+            // #0e172a
 
 export default App;
---
+++ src/pageBlocks/About.jsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <div class="About-body">
+      <h3>About</h3>
+      <p>
+        My name is Hakeem Abdulmalik. I build Full-Stack applications and analyze data. I first got started working with computers when I was in high school. I started an A.S. at my local college with a focus on programming and analysis. I went to a coding bootcamp and now I work as a freelance software engineer. Currently I am starting a degree in Mathematics. I intend on pursing a career in researching Mathematics and Computer Science.
+      </p>
+    </div>
+  )
+}
